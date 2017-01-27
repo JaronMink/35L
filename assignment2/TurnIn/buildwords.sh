a48a68493d67b0b83ecd3d1eef35698800a66f82
JaronMink <jaronmink@gmail.com>
2017-01-26 23:40:07 -0800

    Delete buildwords.sh

--- assignment2/TurnIn/buildwords.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-head -n 985 | tail -n 957 | egrep '<td>.+<\/td>' |
-sed -n '1~2!p' | sed 's/.*<td>\(.*\)<\/td>$/\1/g' |
-sed 's/<u>//g' | sed 's/<\/u>//g' | tr '`' "'" |
-tr -s ', ' '[\n*]' | tr -s '[:upper:]' '[:lower:]' | sort -u
